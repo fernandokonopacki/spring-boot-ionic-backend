5a5ce8ea5538c0ec183927db1fca3985eb1f329f
fernandokonopacki <fernando.konopacki@gmail.com>
2021-06-05 03:27:43 -0300

    Pedido Estado pagamento e Pagamento

---
+++ src/main/java/com/luisfernando/cursomc/repositories/PedidoRepository.java
@@ -0,0 +1,11 @@
+package com.luisfernando.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.luisfernando.cursomc.domain.Pedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
+
+}
