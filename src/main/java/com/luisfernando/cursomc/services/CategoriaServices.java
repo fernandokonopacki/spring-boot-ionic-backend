ca80b1afb20414654f4840f03a410d9880be91de
fernandokonopacki <fernando.konopacki@gmail.com>
2021-06-06 02:04:38 -0300

    Validação Sintatica com Bean Validation

--- src/main/java/com/luisfernando/cursomc/services/CategoriaServices.java
+++ src/main/java/com/luisfernando/cursomc/services/CategoriaServices.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.luisfernando.cursomc.domain.Categoria;
+import com.luisfernando.cursomc.dto.CategoriaDTO;
 import com.luisfernando.cursomc.repositories.CategoriaRepository;
 import com.luisfernando.cursomc.services.exceptions.DataIntegrityException;
 import com.luisfernando.cursomc.services.exceptions.ObjectNotFoundException;
@@ -56,6 +57,10 @@ public class CategoriaServices {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 
 }
