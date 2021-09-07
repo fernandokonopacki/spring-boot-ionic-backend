2f2022b36b82f765d1e10b81e732d07191cb02b6
fernandokonopacki <fernando.konopacki@gmail.com>
2021-09-07 12:16:45 -0300

    criando profiles de teste e desenvolvimento

---
+++ src/main/java/com/luisfernando/cursomc/config/DevConfig.java
@@ -0,0 +1,31 @@
+package com.luisfernando.cursomc.config;
+
+import com.luisfernando.cursomc.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-aut}")
+    private String stategy;
+
+    @Bean
+    public boolean intantiateDatabase() throws ParseException {
+
+        if(!"create".equals(stategy)){
+            return false;
+        }
+        dbService.instantiateTestDatabase();
+        return true;
+    }
+}
